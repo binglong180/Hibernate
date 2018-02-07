5a85b083d18f4c51536c91db168666ed51dd960c
binglong180 <907555993@qq.com>
2018-02-07 17:27:44 +0800

    配置Hibernate的相关文件

---
+++ src/com/niu/hib/pojo/Dept.java
@@ -0,0 +1,28 @@
+package com.niu.hib.pojo;
+
+public class Dept {
+	
+	private Byte deptno;
+	private String dname;
+	private String loc;
+	public Byte getDeptno() {
+		return deptno;
+	}
+	public void setDeptno(Byte deptno) {
+		this.deptno = deptno;
+	}
+	public String getDname() {
+		return dname;
+	}
+	public void setDname(String dname) {
+		this.dname = dname;
+	}
+	public String getLoc() {
+		return loc;
+	}
+	public void setLoc(String loc) {
+		this.loc = loc;
+	}
+	
+
+}
